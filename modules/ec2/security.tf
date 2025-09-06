00cbd59abe475da3464cb6892db94acfaf3b7b9e
Bousselat Ahmed Moncef <a.m.bousselat@gmail.com>
2025-09-06 15:29:46 +0200

    fixing ci/cd pipeline

--- modules/ec2/security.tf
+++ modules/ec2/security.tf
@@ -53,5 +53,5 @@ resource "aws_key_pair" "bastion_key" {
 	count = var.use_bastion ? 1 : 0
 	
 	key_name   = "bastion-key"
-	public_key = file("~/.ssh/id_rsa.pub")
+	public_key = file("${path.module}/keys/id_rsa.pub")
 }
